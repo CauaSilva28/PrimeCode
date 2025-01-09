2a2c91710e453ae7b1252e913b1e9253eb9a1c04
Cauã Silva <127631052+CauaSilva28@users.noreply.github.com>
2025-01-08 22:57:31 -0300

    finalizando css e html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 AOS.init();
+
+function headerControl(){
+    let header = document.querySelector("header");
+
+    let lastScrollTop = 0;
+
+    window.addEventListener('scroll', function (event) {
+
+        // mesma posição
+        if (event.scrollY === lastScrollTop) return;
+
+        if(this.scrollY < lastScrollTop){
+            header.style.opacity = "100%";
+            header.style.position = "fixed";
+        }
+        else{
+            header.style.opacity = "0%";
+            header.style.position = "absolute";
+        }
+
+        lastScrollTop = this.scrollY;
+
+    }, true);
+
+}
+
+headerControl();
